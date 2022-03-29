adbe9e942a69da7a7602d06fb7beea319bd22d29
Alex <alexbachurin91@gmail.com>
2022-03-29 15:40:09 +0300

    add to list functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import './App.scss';
 import InputField from './components/InputField';
-
+import { Todo } from './components/Model';
 
 
 const App: React.FC = () => {
-  const [task, setTask] = useState("");
-  console.log(task)
+  const [task, setTask] = useState<string>("");
+  const [todoS, setTodoS] = useState<Todo[]>([]);
+  console.log(todoS)
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    //check if something inside input
+    if (task) {
+      //add to state
+      setTodoS([...todoS, {id: Date.now(), task, isDone: false}]);
+      setTask('')
+    }
+  }
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
-      <InputField task={task} setTask={setTask} />
+      <InputField task={task} setTask={setTask} handleAdd={handleAdd}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './styles.scss'
 interface Props {
     task: string,
-    setTask: React.Dispatch<React.SetStateAction<string>>
+    setTask: React.Dispatch<React.SetStateAction<string>>,
+    handleAdd: (e: React.FormEvent) => void;
 }
-const InputField: React.FC<Props> = ({task, setTask}) => {
+const InputField: React.FC<Props> = ({task, setTask, handleAdd}) => {
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAdd}>
         <input 
             className='input__box' 
             type="input" 
