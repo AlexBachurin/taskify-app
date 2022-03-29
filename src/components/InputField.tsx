0f90f79c7ff3194e58c31c656d4d27c5ddc3cdec
Alex <alexbachurin91@gmail.com>
2022-03-29 15:16:55 +0300

    added sccs to project + form input component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+const InputField = () => {
+  return (
+    <form className='input'>
+        <input className='input__box' type="input" placeholder='Enter a task'/>
+        <button className='input_submit' type='submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
