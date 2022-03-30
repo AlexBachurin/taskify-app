f74e32efa61d2f1a57201c9eaec4cbc228f3b8b2
Alex <alexbachurin91@gmail.com>
2022-03-30 17:19:00 +0300

    delete task functionality

--- src/components/SingleTodoItem.tsx
+++ src/components/SingleTodoItem.tsx
@@ -12,8 +12,8 @@ interface Props {
     key: number
 }
 const SingleTodoItem: React.FC<Props> = ({id, task, isDone, setTodos, todos}) => {
+    //MARK AS DONE
     const handleDone = (itemId : number) => {
-        console.log(itemId)
         //find todo to mark as done, toggle isDone property,return new array
         setTodos(todos.map(item => {
             if(item.id === itemId) {
@@ -23,6 +23,11 @@ const SingleTodoItem: React.FC<Props> = ({id, task, isDone, setTodos, todos}) =>
         }))
         
     }
+    //DELETE TASK
+    const deleteTask = (itemId: number) => {
+        const filtered = todos.filter(item => item.id !== itemId);
+        setTodos(filtered)
+    }
   return (
     <form className='todos__single'>
         <span className={isDone ? 'todos__single-text_strike' : 'todos__single-text'}>
@@ -31,7 +36,7 @@ const SingleTodoItem: React.FC<Props> = ({id, task, isDone, setTodos, todos}) =>
         <div>
             <span className='icon'><AiFillEdit/></span>
             <span  onClick={() => handleDone(id)} className='icon'><MdDone /></span>
-            <span  className='icon'><AiFillDelete /></span>
+            <span  onClick={() => deleteTask(id)} className='icon'><AiFillDelete /></span>
         </div>
     </form>
   )
