8ad7fbad6f0251a2d573a351176114be39a0b126
Alex <alexbachurin91@gmail.com>
2022-03-30 18:57:37 +0300

    fix for completed section working with active todos

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,13 +9,13 @@ interface Props {
     setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>,
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
-const TodoList: React.FC<Props> = ({todos, setTodos, completedTodos}) => {
+const TodoList: React.FC<Props> = ({todos, setTodos, completedTodos, setCompletedTodos}) => {
   return (
       <div className="container">
         <Droppable droppableId ='ActiveList' >
             {
-                (provided) => (
-            <div className="todos" ref={provided.innerRef} {...provided.droppableProps}>
+                (provided, snapshot) => (
+            <div className={`todos ${snapshot.isDraggingOver ? 'dragactive' : ''}`} ref={provided.innerRef} {...provided.droppableProps}>
                 <span className="todos__heading">
                     Active Tasks
                 </span>
@@ -31,14 +31,14 @@ const TodoList: React.FC<Props> = ({todos, setTodos, completedTodos}) => {
         </Droppable>
          <Droppable droppableId ='CompletedList' >
             {
-                (provided) => (
-            <div className="todos completed" ref={provided.innerRef} {...provided.droppableProps}>
+                (provided, snapshot) => (
+            <div className={`todos completed ${snapshot.isDraggingOver ? 'dragcomplete' : ''}`} ref={provided.innerRef} {...provided.droppableProps}>
                 <span className="todos__heading">
                     Completed Tasks
                 </span>
                 {completedTodos.map((item,index) => {
                     return (
-                    <SingleTodoItem index={index} todos={todos} setTodos={setTodos} key={item.id} {...item}/>
+                    <SingleTodoItem index={index} todos={completedTodos} setTodos={setCompletedTodos} key={item.id} {...item}/>
                     )
                 })}
                 {provided.placeholder}
