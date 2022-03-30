1591b9453cfa92b41231458395e5374eeecef15d
Alex <alexbachurin91@gmail.com>
2022-03-30 18:46:51 +0300

    drag and drop logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.scss';
 import InputField from './components/InputField';
 import { Todo } from './components/Model';
 import TodoList from './components/TodoList';
-import {DragDropContext} from 'react-beautiful-dnd'
+import {DragDropContext, DropResult} from 'react-beautiful-dnd'
 
 const App: React.FC = () => {
   const [completedTodos, setCompletedTodos] = useState<Todo[]>([])
@@ -18,8 +18,36 @@ const App: React.FC = () => {
       setTask('')
     }
   }
+  const onDragEnd = (result: DropResult) => {
+      const {source, destination} = result;
+      //if there is no destination just return from func
+      if (!destination) return;
+      //if we drop in same position do nothing
+      if (destination.droppableId === source.droppableId && 
+        destination.index === source.index) return;
+      let add,
+          active = todos,
+          complete = completedTodos;
+      //if it came from ActiveList take this item, and remove it from list
+      //from where it was taken
+      if (source.droppableId === 'ActiveList') {
+        add = active[source.index];
+        active.splice(source.index, 1)
+      } else {
+        add = complete[source.index];
+        complete.splice(source.index, 1)
+      }
+      //when add this item to the ActiveList or CompletedList depending on destination
+      if (destination.droppableId === 'ActiveList') {
+        active.splice(destination.index, 0, add)
+      } else {
+        complete.splice(destination.index, 0, add)
+      }
+      setCompletedTodos(complete);
+      setTodos(active)
+  }
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
       <span className='heading'>Taskify</span>
       <InputField task={task} setTask={setTask} handleAdd={handleAdd}/>
