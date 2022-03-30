2560a2364c890c2df5a88bf0510f2ac76cb07508
Alex <alexbachurin91@gmail.com>
2022-03-30 17:36:58 +0300

    edit task functionality

--- src/components/SingleTodoItem.tsx
+++ src/components/SingleTodoItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AiFillEdit, AiFillDelete} from 'react-icons/ai'
 import {MdDone} from 'react-icons/md'
 import {Todo} from './Model'
@@ -12,6 +12,9 @@ interface Props {
     key: number
 }
 const SingleTodoItem: React.FC<Props> = ({id, task, isDone, setTodos, todos}) => {
+    //edit state
+    const [isEdit, setIsEdit] = useState<boolean>(false)
+    const [editTask, setEditTask] = useState<string>(task)
     //MARK AS DONE
     const handleDone = (itemId : number) => {
         //find todo to mark as done, toggle isDone property,return new array
@@ -28,13 +31,27 @@ const SingleTodoItem: React.FC<Props> = ({id, task, isDone, setTodos, todos}) =>
         const filtered = todos.filter(item => item.id !== itemId);
         setTodos(filtered)
     }
+    //EDIT TASK
+    const editTaskHandler = () => {
+        setIsEdit(true);
+    }
+    const handleEdit = (e: React.FormEvent<HTMLInputElement>) => {
+        setEditTask(e.currentTarget.value)
+    }
+    const changeTask = (e: React.FormEvent) => {
+        e.preventDefault()
+        setIsEdit(false)
+    }
   return (
-    <form className='todos__single'>
-        <span className={isDone ? 'todos__single-text_strike' : 'todos__single-text'}>
-            {task}
+    <form onSubmit={changeTask} className='todos__single'>
+        {isEdit? <input onChange={handleEdit} value={editTask} type="text" placeholder={editTask}/>
+        : <span className={isDone ? 'todos__single-text_strike' : 'todos__single-text'}>
+            {editTask}
         </span>
+        }
+        
         <div>
-            <span className='icon'><AiFillEdit/></span>
+            <span onClick={editTaskHandler} className='icon'><AiFillEdit/></span>
             <span  onClick={() => handleDone(id)} className='icon'><MdDone /></span>
             <span  onClick={() => deleteTask(id)} className='icon'><AiFillDelete /></span>
         </div>
